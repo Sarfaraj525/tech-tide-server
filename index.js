dfbe1a375e1138acd19f8296b935efdc4917e76c
Sarfaraj525 <sarfaraj525@gmail.com>
2024-08-15 22:55:13 +0600

    basic set up

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("Tech tide is running");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Tech tide Server is running on port ${port}`);
+  });
